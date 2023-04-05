6e4d4f27cfdd632588be55ffde8154db4db37335
GEM-404 <mashaephantus2000@gmail.com>
2023-04-05 12:33:53 +0300

    Added all files to repo

---
+++ app.py
@@ -0,0 +1,18 @@
+"""
+This code will help serve the index.html file
+for my resume.
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
